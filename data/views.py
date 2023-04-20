493e5535dedfab098f9b9ce3c293d1801c47397e
demori <andredemori@ime.eb.br>
2023-04-20 11:53:04 -0300

    update sigla

--- data/views.py
+++ data/views.py
@@ -15,7 +15,7 @@ class DistrictDataJsonView(View):
 
 class DistrictData(View):
     def get(self, request):
-        districts = District.objects.filter(name='Porto Alegre')  # districts = District.objects.filter(name='Porto Alegre')
+        districts = District.objects.filter(SIGLA_UF='RS')  # districts = District.objects.filter(name='Porto Alegre')
         district_data = {}
         for district in districts:
             dictionaries = Dictionary.objects.filter(dictionary_city__city=district)
