5fb2ef9cc9ab762616593599df508bbdc4cf014c
demori <andredemori@ime.eb.br>
2023-06-24 17:35:04 -0300

    update

--- recommendation_system/views.py
+++ recommendation_system/views.py
@@ -2,7 +2,7 @@ import smtplib
 from datetime import date
 from email.message import EmailMessage
 import tablib
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -20,6 +20,8 @@ from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
+
+from district.models import *
 
 class Recommendation_systemView(ViewSet):
     authentication_classes = (TokenAuthentication,)
@@ -49,12 +51,29 @@ class Recommendation_systemView(ViewSet):
             aluguel = request.data.get('sliderValue2')
             # Process the state value or make the necessary API calls
 
-            response = HttpResponse()
+            ###RECOMMENDATION ALGORITHM###
+
+            # 1 #
+            selected_districts = District.objects.filter(SIGLA_UF=state)[:4]
+
+            # Convert selected_districts to a list of dictionaries
+            districts_list = [
+                {
+                    'name': district.name,
+                    'CD_MUN': district.CD_MUN,
+                    # Add more fields as needed
+                }
+                for district in selected_districts
+            ]
+
+            # Return the result as a JSON response
+            response_data = {
+                'districts': districts_list
+            }
+            return JsonResponse(response_data)
         else:
             # Handle other HTTP methods or invalid requests
             response = HttpResponse("Method Not Allowed", status=405)
-
-
-        return response
+            return response
 
 
