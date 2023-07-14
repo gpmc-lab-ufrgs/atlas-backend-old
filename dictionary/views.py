49486adf7df80147a2577426f1db4bcbfd20587e
demori <andredemori@ime.eb.br>
2023-07-13 21:42:58 -0300

    update

--- dictionary/views.py
+++ dictionary/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.views import View
 from .models import Dictionary
 
-class DictionaryJsonView(View): # cria o json dos dicionários separados por classificação
+class DictionaryJsonView(View):
     def get(self, request, *args, **kwargs):
         dictionaries = Dictionary.objects.filter(ranking=1, table='city')
         groups = {}
@@ -15,7 +15,7 @@ class DictionaryJsonView(View): # cria o json dos dicionários separados por cla
             group_title = dictionary.new_classification_ptbr
             group_title_en = dictionary.new_classification_en
             if group_title not in groups:
-                groups[group_title] = []
+                groups[group_title] = {'title_english': group_title_en, 'content': []}
             item = {
                 'label': dictionary.name,
                 'title': dictionary.label_ptbr,
@@ -26,14 +26,15 @@ class DictionaryJsonView(View): # cria o json dos dicionários separados por cla
                 'unit': dictionary.unit,
                 'type': dictionary.unit,
             }
-            groups[group_title].append(item)
+            groups[group_title]['content'].append(item)
         data = []
         for group_title, content in groups.items():
-            group_data = {'title': group_title, 'title_english': group_title_en,'content': content}
+            group_data = {'title': group_title, **content}
             data.append(group_data)
         response = JsonResponse(data, json_dumps_params={'ensure_ascii': False}, safe=False)
         response['Content-Type'] = 'application/json; charset=utf-8'
         return response
+
 
 
 
